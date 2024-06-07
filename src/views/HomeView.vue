b0e3634ca042dcb3b5528b9ea962a59eca81e519
Matthew A. NG <ing.matthewalexander@gmail.com>
2024-06-06 22:37:23 -0500

    Dev commit #1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="newPage()">CLICK ME!</button>
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  methods:{
+    newPage(){
+      console.log('hola mundo')
+    }
   }
 }
 </script>
